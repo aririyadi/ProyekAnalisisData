2f7606bd40b7329415e21d414418e6ef942022e1
Wak Leng Gazy <147322531+aririyadi@users.noreply.github.com>
2023-10-28 14:43:22 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -98,7 +98,7 @@ with col1:
     st.metric("Total Order in (2016 - 2018)", value=total_orders)
 
 with col2:
-    total_revenue = format_currency(daily_orders_df.revenue.sum(), "AUD"," ", locale='es_CO') 
+    total_revenue = format_currency(daily_orders_df.revenue.sum(), "AUD", locale='es_CO') 
     st.metric("Total Revenue in (2016 - 2018)", value=total_revenue)
 
 fig, ax = plt.subplots(figsize=(16, 8))
